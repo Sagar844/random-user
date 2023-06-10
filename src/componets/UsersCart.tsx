86545630e11431c20e0d18efdef8d07938f0257a
SaGaR KuMaR <sagarkumar863081@gmail.com>
2023-06-10 09:14:25 +0530

    d

--- src/componets/UsersCart.tsx
+++ src/componets/UsersCart.tsx
@@ -17,7 +17,7 @@ const UsersCart: FC<UserCartProps> = ({ item, setuser, usersDetail }) => {
         className={`${usersDetail?.email == item?.email? color : ""
           } bg-gray-400 my-5 py-4 px-8 rounded-md  cursor-pointer `}
       >
-        <h1 className="text-[#00000099]">{item.gender}</h1>
+        <h1 className="text-[#00000099]">{item.gender} {item.nat}</h1>
         <h1 className="font-bold">{item.name.first}</h1>
         <h1 className="text-[#E16259] text-sm">{item.email}</h1>
       </div>
